5d154091a6adcf62f315d71295b981ea7661cc1e
BehzadE <ehsani.eof@gmail.com>
2021-12-09 13:30:20 +0330

    ammending previous commit

--- internal/model/init.go
+++ internal/model/init.go
@@ -2,10 +2,35 @@ package model
 
 import (
 	"database/sql"
-	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 )
+
+var migration = `
+create table if not exists Archive (
+    ID integer primary key autoincrement,
+    FilePath text unique,
+    FileSize integer,
+    IndexPath text unique,
+    Processed boolean default false
+);
+
+create table if not exists Page (
+    ID integer primary key,
+    Name text,
+    StreamID integer not null,
+    foreign key(StreamID) references Stream(id)
+);
+
+create table if not exists Stream (
+    ID integer primary key autoincrement,
+    ByteBegin integer,
+    ByteEnd integer,
+    ArchiveID integer not null,
+    foreign key(ArchiveID) references Archive(ID),
+    unique(ByteBegin,ArchiveID)
+);
+`
 
 func InitDB(dst string) (*sql.DB, error) {
 	db, err := sql.Open("sqlite3", dst+"/db.sqlite3?_foreign_keys=on&cache=shared")
@@ -13,11 +38,7 @@ func InitDB(dst string) (*sql.DB, error) {
 		return nil, err
 	}
 	db.SetMaxOpenConns(1)
-	query, err := os.ReadFile("createDB.sql")
-	if err != nil {
-		return nil, err
-	}
-	_, err = db.Exec(string(query))
+	_, err = db.Exec(string(migration))
 	if err != nil {
 		return nil, err
 	}
